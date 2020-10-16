7bd3d09c202da26a4cdf4d70d361f31d03e80fcf
eehmull <eehmull@gmail.com>
2020-10-15 21:48:55 -0600

    Mostrar la data del POST con Jinja2

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+transactions = []
 
 @app.route("/", methods=["GET","POST"])
 def home():
-    print(request.form.get("account"))
+    if request.method == "POST":
+        print(request.form)
+        transactions.append(( 
+            request.form.get("date"),
+            float(request.form.get("amount")),
+            request.form.get("account")
+        ))
     return render_template("form.html")
+
+
+@app.route("/transactions")
+def show_transactions():
+    return render_template("transactions.html", entries=transactions)
