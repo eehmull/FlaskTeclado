c965529307af0fbbfe505f94f4ac054df1be0839
eehmull <eehmull@gmail.com>
2020-10-19 23:44:47 -0600

    Agregando psycopg2 + conexión a BD con PostgreSQL

--- app.py
+++ app.py
@@ -1,20 +1,39 @@
 from flask import Flask, render_template, request
+import psycopg2
+
 
 app = Flask(__name__)
 transactions = []
+POSTGRESQL_URI = "postgres://talumxbu:9VKKaz9m3bqB0IKpOCM44_G9rzOkQ5dI@lallah.db.elephantsql.com:5432/talumxbu"
+
+connection = psycopg2.connect(POSTGRESQL_URI)
+
+try:
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute("CREATE TABLE transactions (date TEXT, amount REAL, account TEXT);")
+except psycopg2.errors.DuplicateTable:
+    pass
+
 
 @app.route("/", methods=["GET","POST"])
 def home():
     if request.method == "POST":
         print(request.form)
-        transactions.append(( 
-            request.form.get("date"),
-            float(request.form.get("amount")),
-            request.form.get("account")
-        ))
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute("INSERT INTO transactions VALUES (%s, %s, %s);", (
+                    request.form.get("date"),
+                    float(request.form.get("amount")),
+                    request.form.get("account")
+                ))
     return render_template("form.html")
 
 
 @app.route("/transactions")
 def show_transactions():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM transactions;")
+            transactions = cursor.fetchall()
     return render_template("transactions.html", entries=transactions)
